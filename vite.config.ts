c0ae66d4d448982c9eebf9dc3b5c21434ddab30c
Angellasarus <angelengineersw@gmail.com>
2026-05-24 22:06:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/MaribelApp/',   // ← AGREGAR ESTA LÍNEA
+    base: '/MaribelApp/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
